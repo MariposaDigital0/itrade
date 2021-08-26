57953679b1538110da96557561f12407b65af5b2
MariposaDigital0 <75035405+MariposaDigital0@users.noreply.github.com>
2021-08-26 21:20:58 +0530

    Update settings.py
    changed the database settings

--- itrade/settings.py
+++ itrade/settings.py
@@ -85,13 +85,13 @@ ASGI_APPLICATION = 'itrade.asgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'itrade',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'itrade',
+#         'USER': 'postgres',
+#         'PASSWORD': '1234',
+#         'HOST': 'localhost'
     }
 }
 
